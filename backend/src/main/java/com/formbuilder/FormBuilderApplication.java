676ce410565df3ae77e72ee1558e108d348cd8f7
Darshan Zatakiya STTL <darshanzatakiya13@gmail.com>
2026-03-11 10:05:25 +0530

    hiding sensitive data

--- backend/src/main/java/com/formbuilder/FormBuilderApplication.java
+++ backend/src/main/java/com/formbuilder/FormBuilderApplication.java
@@ -1,18 +1,11 @@
 package com.formbuilder;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FormBuilderApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
         SpringApplication.run(FormBuilderApplication.class, args);
     }
 }
